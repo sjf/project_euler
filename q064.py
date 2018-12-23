ece09a3c65676a80fe11d7a39e5de26750668a04
sjf <sarah.fortune@gmail.com>
2018-12-23 11:13:22 -0800

    .

---
+++ q064.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from math import sqrt
+N=10_000
+
+def get_sqrt_fraction(n):
+  # https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Algorithm
+  m = 0
+  d = 1
+  a = a0 = int(sqrt(n))
+
+  result = []
+  result.append(a)
+  if a**2 == n:
+    return result
+    
+  while a != 2*a0:
+    m = d*a - m
+    d = (n - m**2)/d
+    a = int((a0 + m)/d)
+    result.append(a)
+  return result
+
+
+count = 0
+for i in range(1,N+1):
+  fraction = get_sqrt_fraction(i)
+  print(i, fraction)
+  if len(fraction[1:]) %2 == 1:
+    count += 1
+
+print(count)
