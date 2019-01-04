ef609dd2ac66eaf47148d60360633bdd8eb98781
sjf <sarah.fortune@gmail.com>
2019-01-04 14:46:06 -0800

    Add unit tests for permutation functions.

--- test_lib.py
+++ test_lib.py
@@ -1,6 +1,17 @@
 import pytest
 from lib import *
 
+#### Permutations ####
+
+def test_permutations():
+  assert permutations([1]) == [[1]]
+  assert permutations([1,2,3]) == [[3, 2, 1], [2, 3, 1], [3, 1, 2], [1, 3, 2], [2, 1, 3], [1, 2, 3]]
+
+def test_combinations():
+  assert combinations([],2) == []
+  assert combinations([1],2) == [[1,1]]
+  assert combinations([1,2,3],2) == [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]]
+
 #### Digit operations ####
 
 def test_num_digits():
